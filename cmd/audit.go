430a1744bd6fbf6d9bebe8d52c3d73cd31177bcc
DARKR <edgeuplink@gmail.com>
2023-03-16 05:12:14 +0800

    turbo mode

--- cmd/audit.go
+++ cmd/audit.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"encoding/json"
 	"fmt"
+	"sync"
+	"time"
 
 	"os"
 	"os/exec"
@@ -19,6 +21,7 @@ var (
 	scanPath  string
 	scanTags  string
 	scanBreak string
+	scanTurbo string
 	fatal     = false
 	warning   = false
 )
@@ -30,31 +33,30 @@ type allStats struct {
 	Fatal int `json:"Fatal"`
 }
 
-type allRules struct {
-	Banner string `yaml:"banner"`
+type Rule struct {
+	ID          int      `yaml:"id"`
+	Name        string   `yaml:"name"`
+	Description string   `yaml:"description"`
+	Solution    string   `yaml:"solution"`
+	Error       string   `yaml:"error"`
+	Type        string   `yaml:"type"`
+	Environment string   `yaml:"environment"`
+	Enforcement bool     `yaml:"enforcement"`
+	Fatal       bool     `yaml:"fatal"`
+	Tags        string   `yaml:"tags,omitempty"`
+	Impact      string   `yaml:"impact,omitempty"`
+	Confidence  string   `yaml:"confidence,omitempty"`
+	Patterns    []string `yaml:"patterns"`
+}
 
-	ExceptionMessage string `yaml:"exceptionmessage"`
+type allRules struct {
+	Banner           string `yaml:"banner"`
+	Rules            []Rule `yaml:"Rules"`
 	ExitCritical     string `yaml:"exitcritical"`
 	ExitWarning      string `yaml:"exitwarning"`
 	ExitClean        string `yaml:"exitclean"`
-
-	Rules []struct {
-		ID          int      `yaml:"id"`
-		Name        string   `yaml:"name"`
-		Description string   `yaml:"description"`
-		Solution    string   `yaml:"solution"`
-		Error       string   `yaml:"error"`
-		Type        string   `yaml:"type"`
-		Environment string   `yaml:"environment"`
-		Enforcement bool     `yaml:"enforcement"`
-		Fatal       bool     `yaml:"fatal"`
-		Tags        string   `yaml:"tags,omitempty"`
-		Impact      string   `yaml:"impact,omitempty"`
-		Confidence  string   `yaml:"confidence,omitempty"`
-		Patterns    []string `yaml:"patterns"`
-	} `yaml:"Rules"`
-
-	Exceptions []int `yaml:"exceptions"`
+	Exceptions       []int  `yaml:"exceptions"`
+	ExceptionMessage string `yaml:"exceptionmessage"`
 }
 
 var (
@@ -126,86 +128,146 @@ var auditCmd = &cobra.Command{
 			os.Exit(0)
 		}
 
-		for _, value := range rules.Rules {
+		if scanTurbo == "false" || len(rules.Rules) < 50 {
 
-			tagfound := FindMatchingString(scanTags, value.Tags, ",")
-			if tagfound || scanTags == "" {
-				fmt.Println("│ ")
-			} else {
-				continue
-			}
+			for _, value := range rules.Rules {
 
-			searchPattern := []byte(strings.Join(value.Patterns, "\n") + "\n")
-			_ = os.WriteFile(searchPatternFile, searchPattern, 0644)
-
-			switch value.Type {
-
-			case "scan":
-
-				fmt.Println("│ ")
-				fmt.Println(line)
-				fmt.Println("│ ")
-				fmt.Println("├ Rule #", value.ID)
-				fmt.Println("│ Rule name : ", value.Name)
-				fmt.Println("│ Rule description : ", value.Description)
-				fmt.Println("│ Impacted Env : ", value.Environment)
-				fmt.Println("│ Confidence : ", value.Confidence)
-				fmt.Println("│ Tags : ", value.Tags)
-				fmt.Println("│ ")
-
-				exception := ContainsInt(rules.Exceptions, value.ID)
-
-				if exception && !auditNox && !value.Enforcement {
-
-					colorRedBold.Println("│")
-					colorRedBold.Println("│ ", rules.ExceptionMessage)
-					colorRedBold.Println("│")
-
+				tagfound := FindMatchingString(scanTags, value.Tags, ",")
+				if tagfound || scanTags == "" {
+					fmt.Println("│ ")
 				} else {
+					continue
+				}
 
-					codePatternScan := []string{"--pcre2", "-p", "-o", "-A0", "-B0", "-C0", "-i", "-U", "-f", searchPatternFile, scanPath}
-					xcmd := exec.Command(rgbin, codePatternScan...)
+				searchPattern := []byte(strings.Join(value.Patterns, "\n") + "\n")
+				_ = os.WriteFile(searchPatternFile, searchPattern, 0644)
+
+				switch value.Type {
+
+				case "scan":
+
+					fmt.Println("│ ")
+					fmt.Println(line)
+					fmt.Println("│ ")
+					fmt.Println("├ Rule #", value.ID)
+					fmt.Println("│ Rule name : ", value.Name)
+					fmt.Println("│ Rule description : ", value.Description)
+					fmt.Println("│ Impacted Env : ", value.Environment)
+					fmt.Println("│ Confidence : ", value.Confidence)
+					fmt.Println("│ Tags : ", value.Tags)
+					fmt.Println("│ ")
+
+					exception := ContainsInt(rules.Exceptions, value.ID)
+
+					if exception && !auditNox && !value.Enforcement {
+
+						colorRedBold.Println("│")
+						colorRedBold.Println("│ ", rules.ExceptionMessage)
+						colorRedBold.Println("│")
+
+					} else {
+
+						codePatternScan := []string{"--pcre2", "-p", "-o", "-A0", "-B0", "-C0", "-i", "-U", "-f", searchPatternFile, scanPath}
+						xcmd := exec.Command(rgbin, codePatternScan...)
+						xcmd.Stdout = os.Stdout
+						xcmd.Stderr = os.Stderr
+						errr := xcmd.Run()
+
+						if errr != nil {
+							if xcmd.ProcessState.ExitCode() == 2 {
+								LogError(errr)
+							} else {
+								colorGreenBold.Println("│ Clean")
+								stats.Clean++
+								stats.Total++
+								fmt.Println("│ ")
+							}
+						} else {
+
+							envfound := FindMatchingString(cfgEnv, value.Environment, ",")
+							if (envfound || strings.Contains(value.Environment, "all") || value.Environment == "") && value.Fatal {
+
+								colorRedBold.Println("│")
+								colorRedBold.Println("│ FATAL : ")
+								colorRedBold.Println("│ ", value.Error)
+								colorRedBold.Println("│")
+								fatal = true
+								stats.Fatal++
+							} else {
+
+								colorRedBold.Println("│")
+								colorRedBold.Println("│")
+								colorRedBold.Println("│ ", value.Error)
+								colorRedBold.Println("│")
+								warning = true
+
+							}
+							colorRedBold.Println("│")
+							colorRedBold.Println("│ Rule : ", value.Name)
+							colorRedBold.Println("│ Target Environment : ", value.Environment)
+							colorRedBold.Println("│ Suggested Solution : ", value.Solution)
+							colorRedBold.Println("│")
+							fmt.Println("│ ")
+							stats.Total++
+							stats.Dirty++
+						}
+
+						jsonOutputFile := strings.Join([]string{pwddir, "/", strconv.Itoa(value.ID), ".json"}, "")
+						jsonoutfile, erroutjson := os.Create(jsonOutputFile)
+						if erroutjson != nil {
+							LogError(erroutjson)
+						}
+						defer jsonoutfile.Close()
+						writer := bufio.NewWriter(jsonoutfile)
+						defer writer.Flush()
+
+						codePatternScanJSON := []string{"--pcre2", "--no-heading", "-o", "-p", "-i", "-U", "--json", "-f", searchPatternFile, scanPath}
+						xcmdJSON := exec.Command(rgbin, codePatternScanJSON...)
+						xcmdJSON.Stdout = jsonoutfile
+						xcmdJSON.Stderr = os.Stderr
+						errrJSON := xcmdJSON.Run()
+
+						if errrJSON != nil {
+							if xcmdJSON.ProcessState.ExitCode() == 2 {
+								LogError(errrJSON)
+							} else {
+								colorGreenBold.Print("│")
+								os.Remove(jsonOutputFile)
+							}
+						} else {
+							ProcessOutput(strings.Join([]string{strconv.Itoa(value.ID), ".json"}, ""), strconv.Itoa(value.ID), value.Name, value.Description, value.Error, value.Solution, value.Fatal)
+							GenerateSarif()
+							colorRedBold.Println("│ ")
+
+						}
+
+					}
+
+				case "collect":
+
+					fmt.Println("│ ")
+					fmt.Println(line)
+					fmt.Println("│ ")
+					fmt.Println("├ Collection : ", value.Name)
+					fmt.Println("│ Description : ", value.Description)
+					fmt.Println("│ Tags : ", value.Tags)
+					fmt.Println("│ ")
+
+					codePatternCollect := []string{"--pcre2", "--no-heading", "-i", "-o", "-U", "-f", searchPatternFile, scanPath}
+					xcmd := exec.Command(rgbin, codePatternCollect...)
 					xcmd.Stdout = os.Stdout
 					xcmd.Stderr = os.Stderr
-					errr := xcmd.Run()
+					err := xcmd.Run()
 
-					if errr != nil {
+					if err != nil {
 						if xcmd.ProcessState.ExitCode() == 2 {
-							LogError(errr)
+							LogError(err)
 						} else {
 							colorGreenBold.Println("│ Clean")
-							stats.Clean++
-							stats.Total++
 							fmt.Println("│ ")
 						}
 					} else {
-
-						envfound := FindMatchingString(cfgEnv, value.Environment, ",")
-						if (envfound || strings.Contains(value.Environment, "all") || value.Environment == "") && value.Fatal {
-
-							colorRedBold.Println("│")
-							colorRedBold.Println("│ FATAL : ")
-							colorRedBold.Println("│ ", value.Error)
-							colorRedBold.Println("│")
-							fatal = true
-							stats.Fatal++
-						} else {
-
-							colorRedBold.Println("│")
-							colorRedBold.Println("│")
-							colorRedBold.Println("│ ", value.Error)
-							colorRedBold.Println("│")
-							warning = true
-
-						}
-						colorRedBold.Println("│")
-						colorRedBold.Println("│ Rule : ", value.Name)
-						colorRedBold.Println("│ Target Environment : ", value.Environment)
-						colorRedBold.Println("│ Suggested Solution : ", value.Solution)
-						colorRedBold.Println("│")
 						fmt.Println("│ ")
-						stats.Total++
-						stats.Dirty++
 					}
 
 					jsonOutputFile := strings.Join([]string{pwddir, "/", strconv.Itoa(value.ID), ".json"}, "")
@@ -217,7 +279,7 @@ var auditCmd = &cobra.Command{
 					writer := bufio.NewWriter(jsonoutfile)
 					defer writer.Flush()
 
-					codePatternScanJSON := []string{"--pcre2", "--no-heading", "-o", "-p", "-i", "-U", "--json", "-f", searchPatternFile, scanPath}
+					codePatternScanJSON := []string{"--pcre2", "--no-heading", "-i", "-o", "-U", "--json", "-f", searchPatternFile, scanPath}
 					xcmdJSON := exec.Command(rgbin, codePatternScanJSON...)
 					xcmdJSON.Stdout = jsonoutfile
 					xcmdJSON.Stderr = os.Stderr
@@ -227,132 +289,97 @@ var auditCmd = &cobra.Command{
 						if xcmdJSON.ProcessState.ExitCode() == 2 {
 							LogError(errrJSON)
 						} else {
-							colorRedBold.Println("│ ")
+							colorGreenBold.Print("│")
+							os.Remove(jsonOutputFile)
 						}
 					} else {
-						ProcessOutput(strings.Join([]string{strconv.Itoa(value.ID), ".json"}, ""), strconv.Itoa(value.ID), value.Name, value.Description, value.Error, value.Solution, value.Fatal)
-						GenerateSarif()
-						colorBlueBold.Println("│ ")
-
+						ProcessOutput(strings.Join([]string{strconv.Itoa(value.ID), ".json"}, ""), strconv.Itoa(value.ID), value.Name, value.Description, "", "", false)
+						colorRedBold.Println("│ ")
 					}
 
+				default:
+
 				}
-
-			case "collect":
-
-				fmt.Println("│ ")
-				fmt.Println(line)
-				fmt.Println("│ ")
-				fmt.Println("├ Collection : ", value.Name)
-				fmt.Println("│ Description : ", value.Description)
-				fmt.Println("│ Tags : ", value.Tags)
-				fmt.Println("│ ")
-
-				codePatternCollect := []string{"--pcre2", "--no-heading", "-i", "-o", "-U", "-f", searchPatternFile, scanPath}
-				xcmd := exec.Command(rgbin, codePatternCollect...)
-				xcmd.Stdout = os.Stdout
-				xcmd.Stderr = os.Stderr
-				err := xcmd.Run()
-
-				if err != nil {
-					if xcmd.ProcessState.ExitCode() == 2 {
-						LogError(err)
-					} else {
-						colorGreenBold.Println("│ Clean")
-						fmt.Println("│ ")
-					}
-				} else {
-					fmt.Println("│ ")
-				}
-
-				jsonOutputFile := strings.Join([]string{pwddir, "/", strconv.Itoa(value.ID), ".json"}, "")
-				jsonoutfile, erroutjson := os.Create(jsonOutputFile)
-				if erroutjson != nil {
-					LogError(erroutjson)
-				}
-				defer jsonoutfile.Close()
-				writer := bufio.NewWriter(jsonoutfile)
-				defer writer.Flush()
-
-				codePatternScanJSON := []string{"--pcre2", "--no-heading", "-i", "-o", "-U", "--json", "-f", searchPatternFile, scanPath}
-				xcmdJSON := exec.Command(rgbin, codePatternScanJSON...)
-				xcmdJSON.Stdout = jsonoutfile
-				xcmdJSON.Stderr = os.Stderr
-				errrJSON := xcmdJSON.Run()
-
-				if errrJSON != nil {
-					if xcmdJSON.ProcessState.ExitCode() == 2 {
-						LogError(errrJSON)
-					} else {
-						colorBlueBold.Println("│")
-					}
-				} else {
-					ProcessOutput(strings.Join([]string{strconv.Itoa(value.ID), ".json"}, ""), strconv.Itoa(value.ID), value.Name, value.Description, "", "", false)
-					colorRedBold.Println("│ ")
-				}
-
-			default:
 
 			}
 
-		}
+			_ = os.Remove(searchPatternFile)
 
-		_ = os.Remove(searchPatternFile)
+			fmt.Println("│")
+			fmt.Println("│")
+			fmt.Println("│")
 
-		fmt.Println("│")
-		fmt.Println("│")
-		fmt.Println("│")
+			colorBold.Println("├  Quick Stats : ")
+			colorBold.Println("│ \t Total Policies Scanned :\t", stats.Total)
+			colorGreenBold.Println("│ \t Clean Policy Checks :\t\t", stats.Clean)
+			colorYellowBold.Println("│ \t Policy Irregularities :\t", stats.Dirty)
+			colorRedBold.Println("│ \t Fatal Policy Breach :\t\t", stats.Fatal)
 
-		colorBold.Println("├  Quick Stats : ")
-		colorBold.Println("│ \t Total Policies Scanned :\t", stats.Total)
-		colorGreenBold.Println("│ \t Clean Policy Checks :\t\t", stats.Clean)
-		colorYellowBold.Println("│ \t Policy Irregularities :\t", stats.Dirty)
-		colorRedBold.Println("│ \t Fatal Policy Breach :\t\t", stats.Fatal)
-
-		jsonstats, _jerr := json.Marshal(stats)
-		if _jerr != nil {
-			LogError(_jerr)
-		}
-
-		_jwerr := os.WriteFile("stats.json", jsonstats, 0644)
-		if _jwerr != nil {
-			LogError(_jwerr)
-		}
-
-		fmt.Println("│")
-		fmt.Println("│")
-		fmt.Println("│")
-		fmt.Println("│")
-
-		if fatal {
-
-			colorRedBold.Println("│")
-			colorRedBold.Println("├ ", rules.ExitCritical)
-			colorRedBold.Println("│")
-			PrintClose()
-			fmt.Println("")
-			if scanBreak != "false" {
-				colorRedBold.Println("► break signal ")
-				os.Exit(1)
+			jsonstats, _jerr := json.Marshal(stats)
+			if _jerr != nil {
+				LogError(_jerr)
 			}
-			os.Exit(0)
 
-		}
+			_jwerr := os.WriteFile("stats.json", jsonstats, 0644)
+			if _jwerr != nil {
+				LogError(_jwerr)
+			}
 
-		if warning {
-			colorYellowBold.Println("│")
-			colorYellowBold.Println("├ ", rules.ExitWarning)
-			colorYellowBold.Println("│")
+			fmt.Println("│")
+			fmt.Println("│")
+			fmt.Println("│")
+			fmt.Println("│")
+
+			if fatal {
+
+				colorRedBold.Println("│")
+				colorRedBold.Println("├ ", rules.ExitCritical)
+				colorRedBold.Println("│")
+				PrintClose()
+				fmt.Println("")
+				if scanBreak != "false" {
+					colorRedBold.Println("► break signal ")
+					os.Exit(1)
+				}
+				os.Exit(0)
+
+			}
+
+			if warning {
+				colorYellowBold.Println("│")
+				colorYellowBold.Println("├ ", rules.ExitWarning)
+				colorYellowBold.Println("│")
+
+			} else {
+
+				colorGreenBold.Println("│")
+				colorGreenBold.Println("├ ", rules.ExitClean)
+				colorGreenBold.Println("│")
+
+			}
 
 		} else {
 
-			colorGreenBold.Println("│")
-			colorGreenBold.Println("├ ", rules.ExitClean)
-			colorGreenBold.Println("│")
-
+			currentTime := time.Now()
+			formattedTime := currentTime.Format("2006-01-02 15:04:05")
+			fmt.Println("├ S ", formattedTime)
+			fmt.Print("│")
+			var wg sync.WaitGroup
+			wg.Add(len(rules.Rules))
+			for _, policy := range rules.Rules {
+				go func(policy Rule) {
+					defer wg.Done()
+					ripTurbo(rgbin, pwddir, scanPath, policy)
+				}(policy)
+			}
+			wg.Wait()
 		}
-
+		currentTime := time.Now()
+		formattedTime := currentTime.Format("2006-01-02 15:04:05")
+		fmt.Println("│")
+		fmt.Println("├ F ", formattedTime)
 		PrintClose()
+
 	},
 }
 
@@ -362,6 +389,7 @@ func init() {
 	auditCmd.PersistentFlags().BoolP("no-exceptions", "x", false, "disables the option to deactivate rules by eXception")
 	auditCmd.PersistentFlags().StringVarP(&scanTags, "tags", "i", "", "include only rules with the specified tag")
 	auditCmd.PersistentFlags().StringVarP(&scanBreak, "break", "b", "true", "disable exit 1 for fatal rules")
+	auditCmd.PersistentFlags().StringVarP(&scanTurbo, "silenturbo", "s", "false", "disable verbose output enabling turbo mode")
 
 	rootCmd.AddCommand(auditCmd)
 
---
+++ cmd/turbo.go
@@ -0,0 +1,50 @@
+package cmd
+
+import (
+	"bufio"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+)
+
+func ripTurbo(rgbin string, pwddir string, scanPath string, policy Rule) {
+
+	jsonOutputFile := strings.Join([]string{pwddir, "/", strconv.Itoa(policy.ID), ".json"}, "")
+	jsonoutfile, erroutjson := os.Create(jsonOutputFile)
+	if erroutjson != nil {
+		LogError(erroutjson)
+	}
+	defer jsonoutfile.Close()
+	writer := bufio.NewWriter(jsonoutfile)
+	defer writer.Flush()
+
+	searchPatternFile := strings.Join([]string{pwddir, "/", "search_regex_", strconv.Itoa(policy.ID)}, "")
+	searchPattern := []byte(strings.Join(policy.Patterns, "\n") + "\n")
+	_ = os.WriteFile(searchPatternFile, searchPattern, 0644)
+
+	codePatternScanJSON := []string{"--pcre2", "--no-heading", "-o", "-p", "-i", "-U", "--json", "-f", searchPatternFile, scanPath}
+
+	xcmdJSON := exec.Command(rgbin, codePatternScanJSON...)
+	xcmdJSON.Stdout = jsonoutfile
+	xcmdJSON.Stderr = os.Stdin
+	errrJSON := xcmdJSON.Run()
+
+	os.Remove(searchPatternFile)
+
+	if errrJSON != nil {
+
+		if xcmdJSON.ProcessState.ExitCode() == 2 {
+			LogError(errrJSON)
+		} else {
+			colorGreenBold.Print("│")
+			os.Remove(jsonOutputFile)
+		}
+
+	} else {
+		ProcessOutput(strings.Join([]string{strconv.Itoa(policy.ID), ".json"}, ""), strconv.Itoa(policy.ID), policy.Name, policy.Description, policy.Error, policy.Solution, policy.Fatal)
+		GenerateSarif()
+		colorRedBold.Print("│")
+	}
+
+}
