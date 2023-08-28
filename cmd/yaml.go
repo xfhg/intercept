8fa50565bad2992d71886be63742884ae3036434
glitch <github@dev.netsec.vip>
2023-08-29 01:36:14 +0800

    yml turbo mode

--- cmd/yaml.go
+++ cmd/yaml.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"path/filepath"
 	"regexp"
+	"sync"
 	"time"
 
 	"github.com/spf13/cobra"
@@ -185,8 +186,119 @@ var yamlCmd = &cobra.Command{
 			}
 
 		} else {
+
+			var wg sync.WaitGroup
+
+			fmt.Println("├─ Scanning.. ─ ─")
+
+			for _, value := range rules.Rules {
+
+				wg.Add(1) // Increment the WaitGroup counter
+
+				go func(value Rule) { // Launch a goroutine with the loop value
+					defer wg.Done() // Decrement the counter when the goroutine completes
+
+					tagfound := FindMatchingString(yamlscanTags, value.Tags, ",")
+					if tagfound || yamlscanTags == "" {
+						fmt.Println("│ ")
+					} else {
+						return
+					}
+
+					switch value.Type {
+
+					case "yml":
+
+						if value.Yml_Filepattern != "" && value.Yml_Structure != "" {
+
+							regex, err := regexp.Compile(value.Yml_Filepattern)
+							if err != nil {
+								LogError(fmt.Errorf("error compiling regex: %s", err))
+								return
+							}
+
+							fileInfo, err := os.Stat(yamlScanPath)
+							if err != nil {
+								LogError(fmt.Errorf("error accessing path: %s", err))
+							}
+
+							// Check if the path is a directory
+							if fileInfo.IsDir() {
+								// fmt.Printf("%s is a directory\n", yamlScanPath)
+							} else {
+								LogError(fmt.Errorf("%s is not a directory", yamlScanPath))
+							}
+
+							// Function to match files
+							err = filepath.Walk(yamlScanPath, func(path string, info os.FileInfo, err error) error {
+								if err != nil {
+									return err
+								}
+								// Ignore directories and match files against regex pattern
+								if !info.IsDir() && regex.MatchString(info.Name()) {
+
+									// fmt.Println("│ Scanning..")
+									// fmt.Println("│ File : " + path) // Print matched file path
+									// filehash, _ := calculateSHA256(path)
+									// fmt.Println("│ Hash : " + filehash)
+									// fmt.Println("│")
+
+									// Read the file in path to a string ymlContent
+
+									ymlContentBytes, err := os.ReadFile(path)
+									if err != nil {
+										LogError(fmt.Errorf("error reading file: %s", err))
+										return nil
+									}
+
+									var yamlObj interface{}
+									err = yaml.Unmarshal(ymlContentBytes, &yamlObj)
+									if err != nil {
+										LogError(fmt.Errorf("error unmarshaling YAML data: %v", err))
+										return nil
+									}
+
+									ymlContent := string(ymlContentBytes)
+
+									xvalid, _ := validateYAMLAndCUEContent(ymlContent, value.Yml_Structure)
+									if xvalid {
+										// fmt.Printf("│ The YAML file is valid.\n")
+										colorGreenBold.Println("│ ✓ ", value.ID, " ", path)
+
+									} else {
+										// fmt.Printf("│ The YAML file is not valid: %s\n", xerrMsg)
+										colorRedBold.Println("│ ✗ ", value.ID, " ", path)
+
+									}
+
+								}
+								return nil
+							})
+							if err != nil {
+
+								LogError(fmt.Errorf("error walking directory: %s", err))
+							}
+
+						} else {
+
+							LogError(errors.New("YML Required policy fields not set (File Pattern / Yml Structure)"))
+
+						}
+					}
+				}(value) // Pass the loop variable to the goroutine
+			}
+
+			wg.Wait() // Wait for all goroutines to finish
+
 		}
 
+		fmt.Println("│")
+		fmt.Println("│")
+		endTime := time.Now()
+		duration := endTime.Sub(startTime)
+		formattedTime = endTime.Format("2006-01-02 15:04:05")
+		fmt.Println("│ Δ ", duration)
+		fmt.Println("├ F ", formattedTime)
 		PrintClose()
 
 	},
