e7cf1c8bc4ab7354feac07c1061ce240e58de58c
Arkaprava Jana <arkaprava.jana@icloud.com>
2024-08-01 16:20:04 +0200

    Commenting existing check to see if the file exists and the subsequent removal as it is not required for unique file names

--- cmd/core_format.go
+++ cmd/core_format.go
@@ -455,10 +455,12 @@ func GenerateComplianceSarif(results InterceptComplianceOutput) {
 	report.AddRun(run)
 
 	sarifOutputFilename := strings.Join([]string{"intercept.", strings.ToLower(results[0].RuleType), ".", strconv.FormatInt(time.Now().UnixNano(), 10), ".sarif.json"}, "")
-
-	if FileExists(sarifOutputFilename) {
-		_ = os.Remove(sarifOutputFilename)
-	}
+	// Commenting existing check to see if the file exists and the subsequent removal as it is not required for unique file names
+	/*
+		if FileExists(sarifOutputFilename) {
+			_ = os.Remove(sarifOutputFilename)
+		}
+	*/
 	if findings > 0 {
 		if err := report.WriteFile(sarifOutputFilename); err != nil {
 			LogError(err)
